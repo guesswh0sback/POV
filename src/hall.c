1806bbea9f5a23176bb9220d36b3c6bb8916e349
Quentin Fallito <quentinfallito@gmail.com>
2025-11-25 11:52:30 +0100

    changed how to compute time for images

--- src/hall.c
+++ src/hall.c
@@ -1,72 +1,46 @@
 #include "hall.h"
 
+int time_per_frame = 50; //ms
+uint8_t known_position = 0; //flag for 
 
-
-
-
-uint16_t ALL_LEDS =       0b1111111111111111;
-uint16_t HALF_LEDS_A =    0b0000000011111111;
-uint16_t HALF_LEDS_B =    0b1111111100000000;
-uint16_t ONE_OUT_OF_TWO = 0b1010101010101010;
-uint16_t JUST_ONE_LED =   0b1000000000000001;
-int counter = 0;
-int protection = 1;
-
-
-
-uint32_t time_diff[100];
-
-
-void HALL_init(){
-    display_bourrin(HALF_LEDS_A, 0.005, 1000);
-
-    DDRD &= ~(1 << PD2); //set PD2 (INT0) as input
-    PORTD |= (1 << PD2); //enable pull-up on PD2
-
-    EIMSK |= (1 << INT0); //enable external interrupt INT0
-    EICRA |= (1 << ISC01) | (1 << ISC00); //trigger INT0 on rising edge
-
-    display_bourrin(HALF_LEDS_B, 0.005, 1000);
+ISR(INT0_vect)
+{
+    display_bourrin(0b1000000000000001, 0.005, 50);
+    INT0_handler();
 }
 
-void TIMER1_init(){
+void HALL_init()
+{
+    // --- TIMER1 as free-running 16-bit counter ---
+    TCCR1A = 0;
+    TCCR1B = (1 << CS11);   // prescaler = 8 → tick = 0.5 µs at 16 MHz
 
-    display_bourrin(0b1010101010101010, 0.005, 1000);
+    // --- INT0 on rising edge ---
+    EICRA = (1 << ISC01) | (1 << ISC00);
+    EIMSK = (1 << INT0);
 
-    TCCR1A = 0; //normal port operation
-    TCCR1B |= (1 << WGM12); //CTC mode
-    TCCR1B |= (1 << CS12); //prescaler 256 (0bXXX<<CS10)
-    OCR1A = 62500; //compare value for 1s at 13MHz with prescaler 256
-    TIMSK1 |= (1 << OCIE1A); //enable compare interrupt
-
-    display_bourrin(0b0101010101010101, 0.005, 1000);
-    
+    sei();
 }
 
-float get_total_time(){
-    uint32_t mean_time = 0;
-    for (int i = 0; i < 100; i++)
+void INT0_handler(){
+    display_index * index = get_display_index(); // function from display
+    if(index->index < index->max_index){ // if display did not have the time to parse all the image
+        time_per_frame -= (int) time_per_frame/2; // lowers the time per frame
+    }
+    else if(index->overflow) // if display parsed to rapidly the image
     {
-        mean_time += time_diff[i];
+        time_per_frame += (int) time_per_frame/2; // increase the time per frame
     }
-    mean_time = mean_time * (256.0 / 13000000.0 / 100.0);
-
-    return ((float)mean_time); //time in seconds
+    known_position = 1; //if interruption is triggered the position is known    
 }
 
-ISR(INT0_vect){
-    if (protection){
-        TCNT1 = 0;  //reset timer
-        counter++;
-        display_bourrin((1<<(counter%16)), 0.005, 1);
-        protection = 0;
+int get_time(){ // return time_per_frame updated by interrupt handler
+    return time_per_frame;
+}
+uint8_t get_known_position(){ // checks if POV is at the known position 
+    if (known_position) //if it is
+    {
+        return known_position--; //return 1 because handler set flag and then resets it to 0
     }
-    else{
-        if (counter <100)
-        {
-            time_diff[counter] = TCNT1;
-        }
-        
-        protection = 1;
-    }
+    return 0; //return 0 if it is not 
 }
--- src/hall.h
+++ src/hall.h
@@ -1,15 +1,20 @@
 #ifndef HALL_H
 #define HALL_H
 
+#include "uart_basic.h"
+#include "display.h"
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-
+#include <stdio.h>
 #include "driver.h"
 
 void HALL_init();
-void TIMER1_init();
-float get_total_time();
+void INT0_handler();
+
+int get_time();
+uint8_t get_known_position();
+
 
 #endif /* HALL_H */
