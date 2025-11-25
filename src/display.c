58c54bfa3f003a3bd9e87df0ac67f0299d13f44a
Quentin Fallito <quentinfallito@gmail.com>
2025-11-25 11:53:06 +0100

    using new hall functions to display images

--- src/display.c
+++ src/display.c
@@ -1,13 +1,38 @@
 #include "display.h"
 
-float time_compute(uint16_t image[]){
-    float total_time = get_total_time();
-    return (total_time / (sizeof(image)));
+display_index * DISPLAY_INDEX;
+
+
+void set_display_index(display_index * INDEX, int max){
+    INDEX->index = 0; // reset index to the begining
+    INDEX->max_index = max; // set max
+    INDEX->overflow = 0; // reset overflow flag
+    INDEX->time = get_time(); // use hall function do set a new time per frame
 }
 
-
-void display_image(uint16_t image[], float delay_time){
-        for (int i =0; i<sizeof(image)-1; i++){
-            display_bourrin(image[i], delay_time, 0.005);
+void check_INDEX(display_index * INDEX){
+    if (INDEX->max_index<INDEX->index) //handles overflow
+    {
+        INDEX->overflow = 1; // set overflow flag
+        INDEX->index = INDEX->max_index; // cap index to avoid going out og the image
     }
+    if (get_known_position) // back to a position where the image can begin anew
+    {
+        set_display_index(INDEX, INDEX->max_index);
+    }
+    
+    
+}
+
+void display_image(uint16_t image[], display_index * INDEX){
+    while (!INDEX->overflow) // equivalent to a while true because of check_index
+    {
+        check_INDEX(INDEX);
+        display_bourrin(image[INDEX->index], 0.0005, INDEX->time); // display current frame
+        INDEX->index ++;
+    }  
+}
+
+display_index * get_display_index(){ // used for hall functions
+    return DISPLAY_INDEX;
 }
--- src/display.h
+++ src/display.h
@@ -5,10 +5,26 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include "driver.h"
-float time_compute(uint16_t image[]);
-void display_image(uint16_t image[], float delay_time);
 
+#include "driver.h"
+#include "hall.h"
+
+
+typedef struct
+{
+    int index;
+    int max_index;
+    uint8_t overflow;
+    int time;
+} display_index;
+
+void set_display_index( display_index * INDEX, int max);
+
+void check_index(display_index * INDEX);
+
+void display_image(uint16_t image[], display_index * INDEX);
+
+display_index * get_display_index(); //used for hall functions
 
 
 #endif
